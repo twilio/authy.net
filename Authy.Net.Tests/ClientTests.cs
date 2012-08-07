db668e438d074b25c3a40a495a4ce549ab150cf4
Devin Martin <devnull@localhost>
2012-08-06 18:05:52 -0600

    Create Basic Registration test that passes.  Move the api key into its own file

--- Authy.Net.Tests/ClientTests.cs
+++ Authy.Net.Tests/ClientTests.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Configuration;
+using System.IO;
 
 namespace Authy.Net.Tests
 {
     /// <remarks>
-    /// In order for these tests to pass, the configuration must be updated with a valid API key
+    /// 
+    /// Important!!!
+    /// 
+    /// These tests won't pass unless you add a file Called ApiKey.txt to the root of the test project.
+    /// Once created add a single line containing a sandbox api key.
+    /// 
     /// </remarks>
     [TestClass]
     public class ClientTests
@@ -19,6 +25,8 @@ namespace Authy.Net.Tests
             var client = this.GoodApiKeyClient;
             var result = client.RegisterUser("test@test.com", "123-456-7890");
             Assert.AreEqual(AuthyStatus.Success, result.Status);
+            Assert.IsNotNull(result.UserId);
+            Assert.AreNotEqual(string.Empty, result.UserId);
         }
 
         [TestMethod]
@@ -64,7 +72,9 @@ namespace Authy.Net.Tests
 
         private Client GoodApiKeyClient
         {
-            get { return new Client(ConfigurationManager.AppSettings["ApiKey"], true); }
+            get { return new Client(ApiKey.Value, true); }
         }
+
+        private Lazy<string> ApiKey = new Lazy<string>(() => File.ReadAllText("ApiKey.txt"));
     }
 }
